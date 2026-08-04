0f9c62fc309a0bc85f12075aed40f2cef1b7e0f8
igorger95-cloud <igorger95@gmail.com>
2026-08-04 14:20:15 +0700

    Add data loading function

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,5 +1,28 @@
 import pandas as pd
 
 
-def load_data(path):
-    return pd.read_csv(path)
+def load_data(train_path, test_path):
+    """
+    Load train and test datasets.
+
+    Parameters
+    ----------
+    train_path : str
+        Path to train.csv.
+
+    test_path : str
+        Path to test.csv.
+
+    Returns
+    -------
+    train : pd.DataFrame
+        Training dataset.
+
+    test : pd.DataFrame
+        Test dataset.
+    """
+
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+
+    return train, test
